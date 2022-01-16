bfc4524b20a9ea129b2774b30b443af91c66635b
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-01-15 21:29:53 -0800

    Updated Header

--- components/Header.js
+++ components/Header.js
@@ -9,13 +9,22 @@ const Header = () => {
 
   return (
     <div className="sticky top-0 w-full h-64 bg-primary flex flex-row justify-center items-center px-8 z-50">
-      <div className="flex flex-col justify-center items-center">
-        <div className="h-24 w-24 relative mb-8">
-          <Avatar />
+      <div className="flex flex-col justify-start items-center mt-12">
+        <div className="w-fit flex flex-row justify-center items-center space-x-4 shadow-xl rounded-lg border-2 border-black mb-8 py-3 px-8">
+          <div className="h-24 w-24 relative">
+            <Avatar />
+          </div>
+          <div className="w-32 flex flex-col">
+            <h2 className="text-black font-bold truncate">
+              {user.getUsername()}
+            </h2>
+            <h2 className="text-gray-800 truncate text-sm">
+              {user.get("ethAddress")}
+            </h2>
+          </div>
         </div>
         <h1 className="text-dark text-5xl font-bold mb-2">Good Morning!</h1>
         <p className="text-lg">Say it back</p>
-        {/* <h2 className=" truncate">{user.getUsername()}</h2> */}
       </div>
       <button className="absolute text-xs top-8 left-8" onClick={logout}>
         Logout
