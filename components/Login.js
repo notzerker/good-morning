48ab100645ff78f132418a91b4d5f27d0ea022b1
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-01-15 19:53:07 -0800

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -1,13 +1,11 @@
 import { useMoralis } from "react-moralis";
 import { BounceLoader } from "react-spinners";
-import Message from "./Message";
 
 const Login = () => {
   const { authenticate, isAuthenticating } = useMoralis();
 
   return (
     <>
-      {/* <Message text="Please verify wallet to login" /> */}
       <div className="relative w-full min-h-screen flex flex-col justify-center items-center">
         <h1 className="text-bold text-9xl mb-2 font-bold">Good Morning</h1>
         <p className="mb-12 text-xl">Have a great day. </p>
