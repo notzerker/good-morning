f6cc932640231d1889016f960194f50b08dd7812
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-01-15 19:43:17 -0800

    Update SendMessage.js

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -5,6 +5,13 @@ const SendMessage = ({ endOfMessagesRef }) => {
   const { user, Moralis } = useMoralis();
   const message = "gm";
 
+  const scrollTo = (ref) => {
+    if (!ref.current) return;
+    else {
+      endOfMessagesRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   const sendMessage = () => {
     const Messages = Moralis.Object.extend("Messages");
     const messages = new Messages();
@@ -23,9 +30,9 @@ const SendMessage = ({ endOfMessagesRef }) => {
           console.log(error.message);
         }
       );
-  };
 
-  // endOfMessagesRef.current.scrollIntoView({ behavior: "smooth" });
+    scrollTo(endOfMessagesRef);
+  };
 
   return (
     <div className="fixed bottom-12 z-50">
