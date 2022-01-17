5ca83ec621586588302a1d7819da04d3f2a4f1ff
AlbertLe <88624470+treblale@users.noreply.github.com>
2022-01-17 02:17:38 -0800

    Added wallectconnect option

---
+++ components/Connect.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useMoralis } from "react-moralis";
+import { BiArrowBack } from "react-icons/bi";
+
+const Connect = ({ setConnect }) => {
+  const { authenticate, isAuthenticating } = useMoralis();
+
+  return (
+    <div>
+      <div
+        className="flex flex-row space-x-2 cursor-pointer items-center justify-start mb-4"
+        onClick={() => setConnect(true)}
+      >
+        <BiArrowBack />
+        <p>Back</p>
+      </div>
+      {/* <p className="mb-2">Login with wallet to enter.</p> */}
+      <button className="w-64 mb-2 justify-start" onClick={authenticate}>
+        <img className="w-4 h-4" src="/metamask.svg" alt="" />
+        <p>Metamask</p>
+      </button>
+      <button
+        className="w-64 justify-start mb-20"
+        onClick={() => authenticate({ provider: "walletconnect" })}
+      >
+        <img className="w-4 h-4" src="/walletconnect.svg" alt="" />
+        <p>Wallet Connect</p>
+      </button>
+    </div>
+  );
+};
+
+export default Connect;
--- components/Login.js
+++ components/Login.js
@@ -1,22 +1,37 @@
 import { useMoralis } from "react-moralis";
 import { BounceLoader } from "react-spinners";
 import { IoIosPartlySunny } from "react-icons/io";
+import { BsArrowRightShort } from "react-icons/bs";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { useState } from "react";
+import Connect from "./Connect";
 
 const Login = () => {
   const { authenticate, isAuthenticating } = useMoralis();
+  const [connect, setConnect] = useState(false);
 
   return (
     <>
-      <div className="relative w-full min-h-screen flex flex-col justify-center items-center dark:bg-dark bg-primary text-black dark:text-white">
+      <div className="relative w-full min-h-screen flex flex-col justify-center items-center dark:bg-dark text-black dark:text-white">
         <IoIosPartlySunny className="text-9xl mb-10" />
-        <h1 className="text-bold text-7xl text-center lg:text-9xl mb-2 font-bold">
-          Good Morning
-        </h1>
-        <p className="mb-12 text-xl text-center">Have a good day.</p>
-        <button className="w-64 mb-20" onClick={authenticate}>
-          <p>Connect Wallet</p>
-          {isAuthenticating ? <BounceLoader color="#000000" size={20} /> : ""}
-        </button>
+        {connect ? (
+          <>
+            <h1 className="text-bold text-7xl text-center lg:text-8xl mb-2 font-bold">
+              Good Morning
+            </h1>
+            <p className="mb-12 text-xl text-center">Have a good day.</p>
+            <button className="w-64 mb-20" onClick={() => setConnect(false)}>
+              <p>Connect Wallet</p>
+              {isAuthenticating ? (
+                <BounceLoader color="#000000" size={20} />
+              ) : (
+                ""
+              )}
+            </button>
+          </>
+        ) : (
+          <Connect setConnect={setConnect} />
+        )}
       </div>
     </>
   );
